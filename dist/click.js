860bfab1d7dbc11c9cf57a08f9e443054a68c5a8
simpart <taria.ki4m@gmail.com>
2017-02-12 17:49:22 +0900

    support mofron v0.6.x

--- dist/click.js
+++ dist/click.js
@@ -11,16 +11,16 @@
 
 /******/ 		// Create a new module (and put it into the cache)
 /******/ 		var module = installedModules[moduleId] = {
-/******/ 			exports: {},
-/******/ 			id: moduleId,
-/******/ 			loaded: false
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
 /******/ 		};
 
 /******/ 		// Execute the module function
 /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
 
 /******/ 		// Flag the module as loaded
-/******/ 		module.loaded = true;
+/******/ 		module.l = true;
 
 /******/ 		// Return the exports of the module
 /******/ 		return module.exports;
@@ -33,74 +33,101 @@
 /******/ 	// expose the module cache
 /******/ 	__webpack_require__.c = installedModules;
 
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ function(module, exports) {
+/***/ (function(module, exports, __webpack_require__) {
 
-	'use strict';
+"use strict";
 
-	var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-	function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-	function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
-	/**
-	 * @file Click.js
-	 * @author simpart
-	 */
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-	/**
-	 * @class mofron.event.Click
-	 * @brief click event class for component
-	 */
-	mofron.event.Click = function (_mofron$event$Base) {
-	    _inherits(_class, _mofron$event$Base);
+/**
+ * @file Click.js
+ * @author simpart
+ */
 
-	    function _class() {
-	        _classCallCheck(this, _class);
+/**
+ * @class mofron.event.Click
+ * @brief click event class for component
+ */
+mofron.event.Click = function (_mofron$Event) {
+    _inherits(_class, _mofron$Event);
 
-	        return _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).apply(this, arguments));
-	    }
+    function _class() {
+        _classCallCheck(this, _class);
 
-	    _createClass(_class, [{
-	        key: 'eventFunc',
+        return _possibleConstructorReturn(this, (_class.__proto__ || Object.getPrototypeOf(_class)).apply(this, arguments));
+    }
 
-	        /**
-	         * add click event to target component.
-	         */
-	        value: function eventFunc() {
-	            try {
-	                var _func = this.func;
-	                var _parm = this.parm;
-	                this.target.getDom().addEventListener('click', function () {
-	                    try {
-	                        if (null != _func) {
-	                            _func(_parm);
-	                        }
-	                    } catch (e) {
-	                        console.error(e.stack);
-	                        throw e;
-	                    }
-	                }, false);
-	            } catch (e) {
-	                console.error(e.stack);
-	                throw e;
-	            }
-	        }
-	    }]);
+    _createClass(_class, [{
+        key: 'eventConts',
 
-	    return _class;
-	}(mofron.event.Base);
+        /**
+         * add click event to target component.
+         */
+        value: function eventConts() {
+            try {
+                var _func = this.func;
+                var _parm = this.parm;
+                this.target.getRawDom().addEventListener('click', function () {
+                    try {
+                        if (null != _func) {
+                            _func(_parm);
+                        }
+                    } catch (e) {
+                        console.error(e.stack);
+                        throw e;
+                    }
+                }, false);
+            } catch (e) {
+                console.error(e.stack);
+                throw e;
+            }
+        }
+    }]);
 
-/***/ }
+    return _class;
+}(mofron.Event);
+
+/***/ })
 /******/ ]);
--- src/click.js
+++ src/click.js
@@ -7,15 +7,15 @@
  * @class mofron.event.Click
  * @brief click event class for component
  */
-mofron.event.Click = class extends mofron.event.Base {
+mofron.event.Click = class extends mofron.Event {
     /**
      * add click event to target component.
      */
-    eventFunc () {
+    eventConts () {
         try {
             var _func = this.func;
             var _parm = this.parm;
-            this.target.getDom().addEventListener('click',function() {
+            this.target.getRawDom().addEventListener('click',function() {
                 try {
                     if (null != _func) {
                         _func(_parm);
